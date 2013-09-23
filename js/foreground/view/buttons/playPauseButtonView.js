4d4a1121ad4380386e671f381bc24f885df1e002
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2013-09-22 20:43:26 -0700

    Getting comfy with mixins for templates :)

--- js/foreground/view/buttons/playPauseButtonView.js
+++ js/foreground/view/buttons/playPauseButtonView.js
@@ -24,34 +24,26 @@ define([
         
         render: function () {
             
-            this.$el.html(this.template(this.model.toJSON()));
+            this.$el.html(this.template(_.extend(this.model.toJSON(), {
+                //  mixin to be able to use it in templating
+                'PlayerState': PlayerState,
+                'playerState': Player.get('state')
+            })));
+
             this.$el.toggleClass('disabled', !this.model.get('enabled'));
             
-            //  TODO: Doing too much work inside of render. I think this should be delgated to the template, but unsure how.
-            var pauseIcon = $('#PauseIcon');
-            var playIcon = $('#PlayIcon');
-
             //  Update which icon is showing whenever the YouTube player changes playing state.
             var playerState = Player.get('state');
 
             if (playerState === PlayerState.BUFFERING) {
                 this.$el.append(this.loadingSpinnerView.render().el);
-
-                playIcon.hide();
-                pauseIcon.hide();
             } else {
                 // Not buffering, so hide the spinner.
                 this.loadingSpinnerView.remove();
 
                 if (playerState === PlayerState.PLAYING) {
-                    //  Change the music button to the 'Pause' image
-                    pauseIcon.show();
-                    playIcon.hide();
                     this.$el.attr('title', this.pauseTitle);
                 } else {
-                    //  Change the music button to the 'Play' image
-                    pauseIcon.hide();
-                    playIcon.show();
                     this.$el.attr('title', this.playTitle);
                 }
             }
@@ -67,6 +59,8 @@ define([
 
             this.listenTo(this.model, 'change:enabled', this.render);
             this.listenTo(Player, 'change:state', this.render);
+
+            this.render();
         },
         
         tryTogglePlayerState: function() {
