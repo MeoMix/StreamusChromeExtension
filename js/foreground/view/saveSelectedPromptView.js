c1bd7d6a359cad1981fef30c22d446cf4e6acd30
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2013-10-25 18:36:06 -0700

    Still working on selectize

--- js/foreground/view/saveSelectedPromptView.js
+++ js/foreground/view/saveSelectedPromptView.js
@@ -1,8 +1,9 @@
 ﻿define([
     'text!../template/saveSelectedPrompt.htm',
     'genericPromptView',
-    'folders'
-], function (SaveSelectedPromptTemplate, GenericPromptView, Folders) {
+    'folders',
+    'videoSearchResults'
+], function (SaveSelectedPromptTemplate, GenericPromptView, Folders, VideoSearchResults) {
     'use strict';
 
     var SaveSelectedPromptView = GenericPromptView.extend({
@@ -18,6 +19,7 @@
         render: function () {
 
             GenericPromptView.prototype.render.call(this, {
+                'selectedResultsCount': VideoSearchResults.selected().count
             }, arguments);
 
             var playlistOptions = Folders.getActiveFolder().get('playlists').map(function (playlist) {
@@ -27,62 +29,49 @@
                 };
             });
 
-            //this.$el.find('select.submittable').selectize({
-            //    //  If false, items created by the user will not show up as available options once they are unselected.
-            //    persist: false,
-            //    maxItems: 1,
-            //    //  The name of the property to use as the "value" when an item is selected.
-            //    valueField: 'id',
-            //    //  The name of the property to render as an option / item label.
-            //    labelField: 'title',
-            //    //  An array of property names to analyze when filtering options.
-            //    searchField: ['title'],
-            //    options: playlistOptions
-            //});
-            
-            var REGEX_EMAIL = '([a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@' +
-                  '(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)';
 
             this.$el.find('select.submittable').selectize({
+                //  If false, items created by the user will not show up as available options once they are unselected.
                 persist: false,
-                maxItems: null,
-                valueField: 'email',
-                labelField: 'name',
-                searchField: ['name', 'email'],
-                options: [
-                    { email: 'brian@thirdroute.com', name: 'Brian Reavis' },
-                    { email: 'nikola@tesla.com', name: 'Nikola Tesla' },
-                    { email: 'someone@gmail.com' }
-                ],
+                maxItems: 1,
+                mode: 'multi',
+                //  The name of the property to use as the "value" when an item is selected.
+                valueField: 'id',
+                //  The name of the property to render as an option / item label.
+                labelField: 'title',
+                //  An array of property names to analyze when filtering options.
+                searchField: ['title'],
+                options: playlistOptions,
                 render: {
                     item: function (item, escape) {
+                        
                         return '<div>' +
-                            (item.name ? '<span class="name">' + escape(item.name) + '</span>' : '') +
-                            (item.email ? '<span class="email">' + escape(item.email) + '</span>' : '') +
+                            (item.title ? '<span class="name">' + escape(item.title) + '</span>' : '') +
                         '</div>';
+                        
                     },
                     option: function (item, escape) {
-                        var label = item.name || item.email;
-                        var caption = item.name ? item.email : null;
+
+                        var caption = "Caption TODO";
+
                         return '<div>' +
-                            '<span class="label">' + escape(label) + '</span>' +
+                            '<span class="label">' + escape(item.title) + '</span>' +
                             (caption ? '<span class="caption">' + escape(caption) + '</span>' : '') +
                         '</div>';
                     }
                 },
                 create: function (input) {
-                    if ((new RegExp('^' + REGEX_EMAIL + '$', 'i')).test(input)) {
-                        return { email: input };
-                    }
-                    var match = input.match(new RegExp('^([^<]*)\<' + REGEX_EMAIL + '\>$', 'i'));
-                    if (match) {
-                        return {
-                            email: match[2],
-                            name: $.trim(match[1])
+
+                    var createResult = false;
+                    var trimmedInput = $.trim(input);
+                    
+                    if (trimmedInput != '') {
+                        createResult = {
+                            name: trimmedInput
                         };
                     }
-                    alert('Invalid email address.');
-                    return false;
+
+                    return createResult;
                 }
             });
 
