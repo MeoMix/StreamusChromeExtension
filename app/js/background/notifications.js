d54b8cb87a6db45b78c67387793689caffd769c8
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2013-12-16 22:43:02 -0800

    close is deprecated, cancel is preferred

--- app/js/background/notifications.js
+++ app/js/background/notifications.js
@@ -8,21 +8,20 @@
     return {
         //  Expects options: { iconUrl: string, title: string, body: string }
         showNotification: function (options) {
-            
+
             //  Spam actions can open a lot of notifications, really only want one at a time I think.
             if (notification) {
-                notification.close();
+                notification.cancel();
                 clearTimeout(closeNotificationTimeout);
             }
 
             notification = window.webkitNotifications.createNotification(options.iconUrl || '', options.title || '', options.body || '');
-            
             notification.show();
             
             closeNotificationTimeout = setTimeout(function () {
 
                 if (notification !== null) {
-                    notification.close();
+                    notification.cancel();
                     notification = null;
                 }
 
