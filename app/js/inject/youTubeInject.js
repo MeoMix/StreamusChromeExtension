03255feb6088c0660587478f3c9a814ff7fbbf98
Nicholas Kircher <foreverskybound@gmail.com>
2013-12-24 14:30:48 +1100

    Updated the youTubeInject.js file to add button whenever page-load changes

--- app/js/inject/youTubeInject.js
+++ app/js/inject/youTubeInject.js
@@ -1,211 +1,245 @@
 //  This code runs on YouTube pages.
 $(function () {
-    'use strict';
+	'use strict';
 
-    //  Inject CSS here to give it priority over all other CSS loaded on the page.
-    var style = document.createElement('link');
-    style.rel = 'stylesheet';
-    style.type = 'text/css';
-    style.href = chrome.extension.getURL('css/youTubeInject.css');
-    document.head.appendChild(style);
+	//  Inject CSS here to give it priority over all other CSS loaded on the page.
+	var style = document.createElement('link');
+	style.rel = 'stylesheet';
+	style.type = 'text/css';
+	style.href = chrome.extension.getURL('css/youTubeInject.css');
+	document.head.appendChild(style);
 
-    var addButtonWrapper = $('<span>');
-    var youtubeButtonInsertLocation = $('#watch7-secondary-actions');
-    addButtonWrapper.insertBefore(youtubeButtonInsertLocation.children(':first'));
+	//console.log("youtubeInject");
 
-    var addButton = $('<button>', {
-        'class': 'action-panel-trigger yt-uix-button yt-uix-button-text yt-uix-tooltip',
-        title: chrome.i18n.getMessage('addVideoToStreamus'),
-        type: 'button',
-        role: 'button',
-        'data-button-toggle': true,
-        'data-trigger-for': 'action-panel-streamus'
-    });
-    addButton.appendTo(addButtonWrapper);
-    var addButtonContent = $('<span>', {
-        'class': 'yt-uix-button-content',
-        text: chrome.i18n.getMessage('addToStreamus')
-    });
-    addButtonContent.appendTo(addButton);
+	var bodyElem = $("body");
+	var firstLoad = true;
+	var waitingForLoad = false;
 
-    chrome.runtime.sendMessage({
-            method: "getYouTubeInjectClicked"
-        }, function(response) {
-            if (!response.result) {
-                addButton.addClass("notClickedYet");
-            }
-        });
+	var observer = new window.WebKitMutationObserver(function (mutations) {
+		//console.log(mutations);
+		if (firstLoad) {
+			//console.log("firstLoad");
+			firstLoad = false;
+			injectStreamusButtons();
+		}
+		else {
+			if (!waitingForLoad && !bodyElem.hasClass("page-loaded")) {
+				//console.log("waitingForLoad");
+				waitingForLoad = true;
+			}
+			else if (waitingForLoad && bodyElem.hasClass("page-loaded")) {
+				//console.log("page has loaded");
+				injectStreamusButtons();
+			}
+		}
 
-    var streamusActionPanel = $('<div>', {
-        id: 'action-panel-streamus',
-        'class': 'action-panel-content',
-        'data-panel-loaded': true,
-        css: {
-            display: 'none',
-            padding: '18px 20px',
-            width: '600px'
-        }
-    });
+	});
 
-    var youtubePanelInsertLocation = $('#watch7-action-panels');
-    streamusActionPanel.insertBefore(youtubePanelInsertLocation.children(':first'));
+	observer.observe(document.querySelector("body"), {attributes: true, attributeFilter: ["class"]});
 
-    var watchActionsSharePanel = $('<div>', {
-        
-    });
-    watchActionsSharePanel.appendTo(streamusActionPanel);
+	function injectStreamusButtons() {
+		var addButtonWrapper = $('<span>');
+		var youtubeButtonInsertLocation = $('#watch7-secondary-actions');
+		addButtonWrapper.insertBefore(youtubeButtonInsertLocation.children(':first'));
 
-    var sharePanel = $('<div>', {
-        'class': 'share-panel'
-    });
-    sharePanel.appendTo(watchActionsSharePanel);
+		var addButton = $('<button>', {
+			'class': 'action-panel-trigger yt-uix-button yt-uix-button-text yt-uix-tooltip',
+			title: chrome.i18n.getMessage('addVideoToStreamus'),
+			type: 'button',
+			role: 'button',
+			'data-button-toggle': true,
+			'data-trigger-for': 'action-panel-streamus'
+		});
+		addButton.appendTo(addButtonWrapper);
+		var addButtonContent = $('<span>', {
+			'class': 'yt-uix-button-content',
+			text: chrome.i18n.getMessage('addToStreamus')
+		});
+		addButtonContent.appendTo(addButton);
 
-    var sharePanelButtons = $('<div>', {
-        id: 'streamus-panel-buttons',
-        'class': 'share-panel-buttons'
-    });
-    sharePanelButtons.appendTo(sharePanel);
+		chrome.runtime.sendMessage({
+			method: "getYouTubeInjectClicked"
+		}, function (response) {
+			if (!response.result) {
+				addButton.addClass("notClickedYet");
+			}
+		});
 
-    var sharePanelMainButtons = $('<span>', {
-        'class': 'share-panel-main-buttons yt-uix-button-group',
-        'data-button-toggle-group': 'share-panels'
-    });
-    sharePanelMainButtons.appendTo(sharePanelButtons);
+		var streamusActionPanel = $('<div>', {
+			id: 'action-panel-streamus',
+			'class': 'action-panel-content',
+			'data-panel-loaded': true,
+			css: {
+				display: 'none',
+				padding: '18px 20px',
+				width: '600px'
+			}
+		});
 
-    var sharePanelPlaylistSelect = $('<div>', {
-        'class': 'share-panel-playlists-container'
-    });
-    sharePanelPlaylistSelect.appendTo(sharePanel);
+		var youtubePanelInsertLocation = $('#watch7-action-panels');
+		streamusActionPanel.insertBefore(youtubePanelInsertLocation.children(':first'));
 
-    var selectPlaylistButton = $('<button>', {
-        type: 'button',
-        'class': 'share-panel-services yt-uix-button yt-uix-button yt-uix-button-text',
-        'data-button-toggle': true,
-        role: 'button',
-        onclick: function() {
-            return false;
-        }
-    });
+		var watchActionsSharePanel = $('<div>', {
 
-    selectPlaylistButton.appendTo(sharePanelMainButtons);
-    var selectPlaylistContent = $('<span>', {
-        'class': 'yt-uix-button-content',
-        text: chrome.i18n.getMessage('selectPlaylist')
-    });
+		});
+		watchActionsSharePanel.appendTo(streamusActionPanel);
 
-    selectPlaylistContent.appendTo(selectPlaylistButton);
+		var sharePanel = $('<div>', {
+			'class': 'share-panel'
+		});
+		sharePanel.appendTo(watchActionsSharePanel);
 
-    var successEventNotification = $('<div>', {
-        id: 'successEventNotification',
-        text: chrome.i18n.getMessage('videoAddSuccess'),
-        'class': 'eventNotification'
-    });
-    successEventNotification.appendTo(sharePanelMainButtons);
+		var sharePanelButtons = $('<div>', {
+			id: 'streamus-panel-buttons',
+			'class': 'share-panel-buttons'
+		});
+		sharePanelButtons.appendTo(sharePanel);
 
-    var errorEventNotification = $('<div>', {
-        id: 'errorEventNotification',
-        text: chrome.i18n.getMessage('errorEncountered'),
-        'class': 'eventNotification'
-    });
-    errorEventNotification.appendTo(sharePanelMainButtons);
+		var sharePanelMainButtons = $('<span>', {
+			'class': 'share-panel-main-buttons yt-uix-button-group',
+			'data-button-toggle-group': 'share-panels'
+		});
+		sharePanelMainButtons.appendTo(sharePanelButtons);
 
-    var playlistSelect = $('<select>', {
-        id: 'playlistSelect',
-        'class': 'yt-uix-form-input-text share-panel-url'
-    });
+		var sharePanelPlaylistSelect = $('<div>', {
+			'class': 'share-panel-playlists-container'
+		});
+		sharePanelPlaylistSelect.appendTo(sharePanel);
 
-    playlistSelect.appendTo(sharePanelPlaylistSelect);
+		var selectPlaylistButton = $('<button>', {
+			type: 'button',
+			'class': 'share-panel-services yt-uix-button yt-uix-button yt-uix-button-text',
+			'data-button-toggle': true,
+			role: 'button',
+			onclick: function () {
+				return false;
+			}
+		});
 
-    var videoAddButton = $('<input>', {
-        type: 'button',
-        value: chrome.i18n.getMessage('addVideo'),
-        title: chrome.i18n.getMessage('addVideo'),
-        id: 'streamusVideoAddButton',
-        'class': 'yt-uix-button yt-uix-tooltip',
-        click: function() {
+		selectPlaylistButton.appendTo(sharePanelMainButtons);
+		var selectPlaylistContent = $('<span>', {
+			'class': 'yt-uix-button-content',
+			text: chrome.i18n.getMessage('selectPlaylist')
+		});
 
-            $(this).val(chrome.i18n.getMessage('working'));
-            $(this).attr('disabled', true);
+		selectPlaylistContent.appendTo(selectPlaylistButton);
 
-            var match = document.URL.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|watch\?.*?\&v=)([^#\&\?]*).*/);
-            var videoId = (match && match[2].length === 11) ? match[2] : null;
+		var successEventNotification = $('<div>', {
+			id: 'successEventNotification',
+			text: chrome.i18n.getMessage('videoAddSuccess'),
+			'class': 'eventNotification'
+		});
+		successEventNotification.appendTo(sharePanelMainButtons);
 
-            var playlistId = playlistSelect.val();
+		var errorEventNotification = $('<div>', {
+			id: 'errorEventNotification',
+			text: chrome.i18n.getMessage('errorEncountered'),
+			'class': 'eventNotification'
+		});
+		errorEventNotification.appendTo(sharePanelMainButtons);
 
-            var self = this;
-            chrome.runtime.sendMessage({
-                    method: "addVideoByIdToPlaylist",
-                    playlistId: playlistId,
-                    videoId: videoId
-                }, function(response) {
+		var playlistSelect = $('<select>', {
+			id: 'playlistSelect',
+			'class': 'yt-uix-form-input-text share-panel-url'
+		});
 
-                    if (response.result === 'success') {
-                        $(self).removeAttr('disabled');
-                        $(self).val(chrome.i18n.getMessage('addVideo'));
-                        successEventNotification.fadeIn().css("display", "inline-block");
+		playlistSelect.appendTo(sharePanelPlaylistSelect);
 
-                        setTimeout(function() {
-                            successEventNotification.fadeOut();
-                        }, 3000);
-                    } else {
-                        $(self).removeAttr('disabled');
-                        $(self).val(chrome.i18n.getMessage('addVideo'));
-                        errorEventNotification.fadeIn().css("display", "inline-block");
-                        setTimeout(function() {
-                            errorEventNotification.fadeOut();
-                        }, 3000);
-                    }
+		var videoAddButton = $('<input>', {
+			type: 'button',
+			value: chrome.i18n.getMessage('addVideo'),
+			title: chrome.i18n.getMessage('addVideo'),
+			id: 'streamusVideoAddButton',
+			'class': 'yt-uix-button yt-uix-tooltip',
+			click: function () {
+
+				$(this).val(chrome.i18n.getMessage('working'));
+				$(this).attr('disabled', true);
+
+				var match = document.URL.match(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|watch\?.*?\&v=)([^#\&\?]*).*/);
+				var videoId = (match && match[2].length === 11) ? match[2] : null;
+
+				var playlistId = playlistSelect.val();
+
+				var self = this;
+				chrome.runtime.sendMessage({
+					method: "addVideoByIdToPlaylist",
+					playlistId: playlistId,
+					videoId: videoId
+				}, function (response) {
+
+					if (response.result === 'success') {
+						$(self).removeAttr('disabled');
+						$(self).val(chrome.i18n.getMessage('addVideo'));
+						successEventNotification.fadeIn().css("display", "inline-block");
+
+						setTimeout(function () {
+							successEventNotification.fadeOut();
+						}, 3000);
+					} else {
+						$(self).removeAttr('disabled');
+						$(self).val(chrome.i18n.getMessage('addVideo'));
+						errorEventNotification.fadeIn().css("display", "inline-block");
+						setTimeout(function () {
+							errorEventNotification.fadeOut();
+						}, 3000);
+					}
 
 
-                });
-        }
-    });
-    videoAddButton.appendTo(sharePanelPlaylistSelect);
+				});
+			}
+		});
+		videoAddButton.appendTo(sharePanelPlaylistSelect);
 
-    selectPlaylistButton.click(function() {
-        sharePanelPlaylistSelect.removeClass('hid');
-    });
+		selectPlaylistButton.click(function () {
+			sharePanelPlaylistSelect.removeClass('hid');
+		});
 
-    chrome.runtime.sendMessage({ method: "getFolders" }, function(getFoldersResponse) {
+		chrome.runtime.sendMessage({ method: "getFolders" }, function (getFoldersResponse) {
 
-        var folders = getFoldersResponse.folders;
+			var folders = getFoldersResponse.folders;
 
-        if (folders.length === 1) {
+			if (folders.length === 1) {
 
-            selectPlaylistButton.addClass('yt-uix-button-toggled');
-            sharePanelPlaylistSelect.removeClass('hid');
+				selectPlaylistButton.addClass('yt-uix-button-toggled');
+				sharePanelPlaylistSelect.removeClass('hid');
 
-            for (var i = 0; i < folders[0].playlists.length; i++) {
-                $('<option>', {
-                    value: folders[0].playlists[i].id,
-                    text: folders[0].playlists[i].title
-                }).appendTo(playlistSelect);
-            }
+				for (var i = 0; i < folders[0].playlists.length; i++) {
+					$('<option>', {
+						value: folders[0].playlists[i].id,
+						text: folders[0].playlists[i].title
+					}).appendTo(playlistSelect);
+				}
 
-        }
+			}
 
-    });
+		});
 
-    chrome.runtime.onMessage.addListener(function(request) {
-        switch (request.event) {
-        case 'add':
-            var playlistOption = $('<option>', {
-                value: request.data.id,
-                text: request.data.title
-            });
+		chrome.runtime.onMessage.addListener(function (request) {
+			switch (request.event) {
+				case 'add':
+					var playlistOption = $('<option>', {
+						value: request.data.id,
+						text: request.data.title
+					});
 
-            playlistOption.appendTo(playlistSelect);
-            break;
-        case 'remove':
-            playlistSelect.find('option[value="' + request.data.id + '"]').remove();
-            break;
-        case 'rename':
-            playlistSelect.find('option[value="' + request.data.id + '"]').text(request.data.title);
-            break;
-        default:
-            console.error("Unhandled request", request);
-            break;
-        }
-    });
+					playlistOption.appendTo(playlistSelect);
+					break;
+				case 'remove':
+					playlistSelect.find('option[value="' + request.data.id + '"]').remove();
+					break;
+				case 'rename':
+					playlistSelect.find('option[value="' + request.data.id + '"]').text(request.data.title);
+					break;
+				default:
+					console.error("Unhandled request", request);
+					break;
+			}
+		});
+	}
+
+
+
+
 
 });
