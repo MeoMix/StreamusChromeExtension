b9acb653e628504706d1a445f850137e16b53d29
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2014-03-17 22:57:46 -0700

    Yesss finally fixed the event unbinding / memory leak issue.

--- src/js/background/background.js
+++ src/js/background/background.js
@@ -45,14 +45,11 @@ define([
     //  I know this sucks. It's because of a 'bug' in chrome extensions where foreground can't reliably unsubscribe all of its events so the background has to be responsible for it. :(
     window.unbindViewEvents = function (foregroundViewType) {
 
-        //var collectionsToUnbind = [StreamItems, VideoSearchResults, Playlists];
-        //var allToUnbind = [Player, User, NextButton, PreviousButton, PlayPauseButton, ShuffleButton, RepeatButton, RadioButton];
-
-        var allToUnbind = [];
-        var collectionsToUnbind = [Playlists];
+        var collectionsToUnbind = [StreamItems, VideoSearchResults, Playlists];
+        var allToUnbind = [Player, User, NextButton, PreviousButton, PlayPauseButton, ShuffleButton, RepeatButton, RadioButton];
 
         _.each(collectionsToUnbind, function(collectionToUnbind) {
-            //allToUnbind.push(collectionToUnbind);
+            allToUnbind.push(collectionToUnbind);
 
             collectionToUnbind.each(function (modelToUnbind) {
                 allToUnbind.push(modelToUnbind);
@@ -61,33 +58,24 @@ define([
 
         _.each(allToUnbind, function (toUnbind) {
 
-            console.log("Looking at:", toUnbind);
-
             var viewContexts = [];
 
             _.each(toUnbind._events, function (eventGroup) {
 
                 _.each(_.toArray(eventGroup), function (event) {
-
-                    console.log("event.ctx", event.ctx);
-
+                    
                     if (event.ctx instanceof foregroundViewType) {
                         if (viewContexts.indexOf(event.ctx) === -1) {
-                            console.log("Found new viewContext:", event.ctx);
                             viewContexts.push(event.ctx);
                         }
                     }
-
+                    
                 });
 
             });
 
             _.each(viewContexts, function (viewContext) {
-                //toUnbind.off(null, null, viewContext);
-
-                viewContext.undelegateEvents();
-                //viewContext.close();
-                console.log("undelegateEvents context:", viewContext);
+                viewContext.stopListening();
             });
 
             viewContexts.length = 0;
