afdc7bd84ed1d38fa4aeb6b17184e7a7a9b2a628
Bryon Keck <vertex.bt@gmail.com>
2014-03-21 20:54:10 -0700

    pause on "Watch on Youtube", resolve #169

--- src/js/foreground/view/rightBasePane/streamItemView.js
+++ src/js/foreground/view/rightBasePane/streamItemView.js
@@ -9,16 +9,17 @@
     'foreground/view/deleteButtonView',
     'foreground/view/saveToPlaylistButtonView',
     'foreground/view/playInStreamButtonView',
-    'text!template/streamItem.html'
-], function (Playlists, StreamItems, User, PlayPauseButton, ListItemType, Utility, ContextMenuItems, DeleteButtonView, SaveToPlaylistButtonView, PlayInStreamButtonView, StreamItemTemplate) {
+    'text!template/streamItem.html',
+    'background/model/player'
+], function (Playlists, StreamItems, User, PlayPauseButton, ListItemType, Utility, ContextMenuItems, DeleteButtonView, SaveToPlaylistButtonView, PlayInStreamButtonView, StreamItemTemplate, Player) {
     'use strict';
 
     var StreamItemView = Backbone.Marionette.Layout.extend({
-        
+
         className: 'list-item stream-item multi-select-item',
 
         template: _.template(StreamItemTemplate),
-        
+
         templateHelpers: function () {
             return {
                 hdMessage: chrome.i18n.getMessage('hd'),
@@ -26,7 +27,7 @@
                 instant: this.instant
             };
         },
-        
+
         instant: false,
 
         attributes: function () {
@@ -35,18 +36,18 @@
                 'data-type': ListItemType.StreamItem
             };
         },
-        
+
         events: {
             'contextmenu': 'showContextMenu',
             'dblclick': 'activateOrToggleState'
         },
-        
+
         modelEvents: {
             'change:active': 'setActiveClass',
             'change:selected': 'setSelectedClass',
             'destroy': 'remove'
         },
-        
+
         ui: {
             imageThumbnail: 'img.item-thumb'
         },
@@ -56,7 +57,7 @@
             saveToPlaylistRegion: '.save-to-playlist-region',
             playInStreamRegion: '.play-in-stream-region'
         },
-        
+
         onShow: function () {
             //  If the stream item is active -- ensure it is instantly visible.
             if (this.model.get('active')) {
@@ -70,7 +71,7 @@
         onRender: function () {
             this.setActiveClass();
             this.setSelectedClass();
-            
+
             this.playInStreamRegion.show(new PlayInStreamButtonView({
                 model: this.model.get('video')
             }));
@@ -78,12 +79,12 @@
             this.deleteRegion.show(new DeleteButtonView({
                 model: this.model
             }));
-            
+
             this.saveToPlaylistRegion.show(new SaveToPlaylistButtonView({
                 model: this.model.get('video')
             }));
         },
-            
+
         initialize: function (options) {
             this.instant = options && options.instant !== undefined ? options.instant : this.instant;
         },
@@ -95,18 +96,18 @@
                 PlayPauseButton.tryTogglePlayerState();
             }
         },
- 
+
         //  Force the view to reflect the model's active class. It's important to do this here, and not through render always, because
         //  render will cause the lazy-loaded image to be reset.
         setActiveClass: function () {
             var active = this.model.get('active');
             this.$el.toggleClass('active', active);
-            
+
             if (active) {
                 this.$el.scrollIntoView();
             }
         },
-        
+
         //  TODO: Maybe keep DRY with video search results and playlist items by introducing a "Selectable" list item view.
         setSelectedClass: function () {
             this.$el.toggleClass('selected', this.model.get('selected'));
@@ -117,18 +118,18 @@
             //  Whenever a context menu is shown -- set preventDefault to true to let foreground know to not reset the context menu.
             event.preventDefault();
             var self = this;
-            
+
             var userSignedIn = User.get('signedIn');
 
             var activePlaylist = Playlists.getActivePlaylist();
             var videoAlreadyExists = false;
-            
+
             if (userSignedIn) {
                 videoAlreadyExists = activePlaylist.get('items').videoAlreadyExists(self.model.get('video'));
             }
 
             var saveTitle = '';
-            
+
             if (userSignedIn && videoAlreadyExists) {
                 saveTitle = chrome.i18n.getMessage('duplicatesNotAllowed');
             } else if (!userSignedIn) {
@@ -177,13 +178,16 @@
                 }, {
                     text: chrome.i18n.getMessage('watchOnYouTube'),
                     onClick: function () {
+                        if( Player.get('state') === 1 )
+                        {
+                            Player.pause();
+                        }
 
                         chrome.tabs.create({
-                            url: self.model.get('video').get('url')
+                            url: self.model.get('video').get('url') + '?t=' + Player.get('currentTime') + 's'
                         });
-
-                    }
-                }]
+                      }
+                  }]
             );
 
         }
