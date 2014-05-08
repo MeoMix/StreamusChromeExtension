36f386be00d589603d65dff8de1e4bc57c371fa8
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2014-05-08 16:30:54 -0700

    Added an application object

---
+++ src/js/foreground/application.js
@@ -0,0 +1,20 @@
+﻿define([
+    'foreground/view/behavior/behaviors'
+], function (Behaviors) {
+    'use strict';
+    
+    var Application = new Backbone.Marionette.Application();
+
+    Application.addInitializer(function() {
+        Backbone.Marionette.Behaviors.behaviorsLookup = function () {
+            return Behaviors;
+        };
+    });
+    
+    Application.on('initialize:after', function () {
+        //  Fire up the foreground:
+        require(['foreground/foreground']);
+    });
+
+    Application.start();
+});
--- src/js/foreground/plugins.js
+++ src/js/foreground/plugins.js
@@ -12,13 +12,6 @@
     'selectize'
 ], function () {
     
-    //  TODO: FIX by introducing an Application object for another level of granularity!
-    require(['foreground/view/behavior/behaviors'], function (Behaviors) {
-        Backbone.Marionette.Behaviors.behaviorsLookup = function () {
-            return Behaviors;
-        };
-        
-        //  Finally, load the foreground:
-        require(['foreground/foreground']);
-    });
+    //  Finally, load the application which will initialize the foreground:
+    require(['foreground/application']);
 });
