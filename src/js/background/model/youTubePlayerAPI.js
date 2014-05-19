e8990e3bdcf0705352945b64aaa3a99362a69234
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2014-05-19 13:08:34 -0700

    Trying to fix the iframe API stuff.
    I'm wondering if I need to set the script that iframe API injects to
    defer, as well, or if simply deferring the initial one is sufficient. I
    suspect it would be sufficient, but unsure.

--- src/js/background/model/youTubePlayerAPI.js
+++ src/js/background/model/youTubePlayerAPI.js
@@ -21,48 +21,7 @@ define(function() {
 
                 var refererUrl = info.url.substring(0, info.url.indexOf('/embed/'));
 
-                if (refererRequestHeader === undefined) {
-                    info.requestHeaders.push({
-                        name: 'Referer',
-                        value: refererUrl
-                    });
-                } else {
-                    refererRequestHeader.value = refererUrl;
-                }   
-
-                //  Make Streamus look like an iPhone to guarantee the html5 player shows up even if YouTube has an ad.
-                var userAgentRequestHeader = _.find(info.requestHeaders, function (requestHeader) {
-                    return requestHeader.name === 'User-Agent';
-                });
-
-                var iPhoneUserAgent = 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5';
-                if (userAgentRequestHeader === undefined) {
-                    info.requestHeaders.push({
-                        name: 'User-Agent',
-                        value: iPhoneUserAgent
-                    });
-                } else {
-                    userAgentRequestHeader.value = iPhoneUserAgent;
-                }
-                //  https://www.youtube.com/embed/?enablejsapi=1&amp;origin=chrome-extension%3A%2F%2Fjbnkffmindojffecdhbbmekbmkkfpmjd
-                return { requestHeaders: info.requestHeaders };
-            }, {
-                //urls: ['*://*.youtube.com/embed/?enablejsapi=1&origin=chrome-extension%3A%2F%2Fjbnkffmindojffecdhbbmekbmkkfpmjd']
-                //  Match on my specific iframe or else else this logic can leak into outside webpages and corrupt other YouTube embeds.
-                urls: ['*://*.youtube.com/embed/?enablejsapi=1&origin=chrome-extension:\\\\jbnkffmindojffecdhbbmekbmkkfpmjd']
-            },
-                ['blocking', 'requestHeaders']
-            );
-
-            chrome.webRequest.onBeforeSendHeaders.addListener(function (info) {
-                //  Bypass YouTube's embedded player content restrictions by provided a value for Referer.
-                var refererRequestHeader = _.find(info.requestHeaders, function (requestHeader) {
-                    return requestHeader.name === 'Referer';
-                });
-
-                var refererUrl = info.url.substring(0, info.url.indexOf('/embed/'));
-
-                if (refererRequestHeader === undefined) {
+                if (_.isUndefined(refererRequestHeader)) {
                     info.requestHeaders.push({
                         name: 'Referer',
                         value: refererUrl
@@ -100,10 +59,13 @@ define(function() {
                 this.set('ready', true);
             }.bind(this);
 
-            $('<script>', {
-                src: 'https://www.youtube.com/iframe_api',
-                async: true
-            }).insertBefore($('script:first'));
+            //  Give this as much time as possible to load:
+            setTimeout(function() {
+                $('<script>', {
+                    src: 'https://www.youtube.com/iframe_api',
+                    defer: true
+                }).insertBefore($('script:last'));
+            });
 
         }
     });
