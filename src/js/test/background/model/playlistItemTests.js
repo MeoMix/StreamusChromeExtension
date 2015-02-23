d2f949ef9a7729a9f52ae2ab469159304ca6e47d
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2015-02-22 22:37:10 -0800

    Restoring some tests I accidentally deleted

---
+++ src/js/test/background/model/playlistItemTests.js
@@ -0,0 +1,30 @@
+﻿define(function (require) {
+    'use strict';
+
+    var PlaylistItem = require('background/model/playlistItem');
+    var TestUtility = require('test/testUtility');
+
+    describe('PlaylistItem', function () {
+        it('Should set its title properly when being created with a Song object', function () {
+            var song = TestUtility.buildSong();
+
+            var playlistItem = new PlaylistItem({
+                title: song.get('title'),
+                song: song
+            });
+   
+            expect(playlistItem.get('title')).to.equal(song.get('title'));
+        });
+        
+        it('Should set its title properly when being created with raw Song data', function () {
+            var song = TestUtility.getRawSong();
+
+            var playlistItem = new PlaylistItem({
+                title: song.title,
+                song: song
+            });
+
+            expect(playlistItem.get('title')).to.equal(song.title);
+        });
+    });
+});
---
+++ src/js/test/background/model/relatedSongsManagerTests.js
@@ -0,0 +1,40 @@
+﻿define(function (require) {
+    'use strict';
+
+    var RelatedSongsManager = require('background/model/relatedSongsManager');
+
+    describe('RelatedSongsManager', function () {
+        beforeEach(function () {
+            sinon.stub($, 'ajax');
+            this.relatedSongsManager = new RelatedSongsManager();
+        });
+        
+        afterEach(function () {
+            $.ajax.restore();
+        });
+
+        it('Should be able to get a queue requests for related song information', function () {
+            var requests = [];
+            
+            for (var count = 0; count < 10; count++) {
+                requests.push({
+                    songId: 'btDPtzCGNTE',
+                    success: _.noop
+                });
+            }
+
+            _.each(requests, function (request) {
+                this.relatedSongsManager.getRelatedSongs(request);
+            }, this);
+
+            expect(this.relatedSongsManager.get('requestOptionsQueue').length).to.equal(requests.length - this.relatedSongsManager.get('maxRequestCount'));
+            expect(this.relatedSongsManager.get('currentRequestCount')).to.equal(this.relatedSongsManager.get('maxRequestCount'));
+        });
+
+        it('should not be able to request when currentRequestCount is equal to maxRequestCount', function () {
+            this.relatedSongsManager.set('currentRequestCount', this.relatedSongsManager.get('maxRequestCount'));
+            expect(this.relatedSongsManager._canRequest()).to.equal(false);
+        });
+    });
+});
+
