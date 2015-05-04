8ebef706d4e2cf75605843c54db46d3a46363b1b
jcme <holmes.jc@gmail.com>
2015-05-04 17:03:53 +0100

    Allows space bar to toggle player state

--- src/js/foreground/view/appBar/playPauseButtonView.js
+++ src/js/foreground/view/appBar/playPauseButtonView.js
@@ -2,6 +2,7 @@
     'use strict';
 
     var PlayerState = require('common/enum/playerState');
+    var KeyboardKey = require('foreground/enum/keyboardKey');
     var PlayPauseButtonTemplate = require('text!template/appBar/playPauseButton.html');
     var PauseIconTemplate = require('text!template/icon/pauseIcon_30.svg');
     var PlayIconTemplate = require('text!template/icon/playIcon_30.svg');
@@ -36,6 +37,10 @@
         initialize: function() {
             this.player = Streamus.backgroundPage.player;
             this.listenTo(this.player, 'change:state', this._onPlayerChangeState);
+
+            // It's important to watch for keydown events at the window level so that spacebar can trigger play/pause without requiring any UI element to have focus.
+            this._onKeyDown = this._onKeyDown.bind(this);
+            window.addEventListener('keydown', this._onKeyDown);
         },
 
         onRender: function() {
@@ -44,6 +49,14 @@
 
         _onClick: function() {
             this.model.tryTogglePlayerState();
+        },
+
+        _onKeyDown: function (event) {
+            if (document.activeElement === document.body) {
+                if (event.keyCode == KeyboardKey.Space) {
+                    this._onClick();
+                }
+            }
         },
 
         _onChangeEnabled: function(model, enabled) {
