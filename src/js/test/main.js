10a1a8dd4292bd89e3535f02c0f8fffa05ef6e5d
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2015-05-05 22:27:26 -0700

    Fixing broken test case setup

--- src/js/test/main.js
+++ src/js/test/main.js
@@ -20,14 +20,14 @@
 //    require(['test/plugins']);
 //});
 
-define('../common/requireConfig', function() {
+define(['../common/requireConfig'], function() {
     'use strict';
 
     //  It didn't make sense to put testing information into requireConfig.
     //  So, I mix it into the config object here.
-    requirejs.s.contexts._.config.paths.chai = 'thirdParty/chai';
-    requirejs.s.contexts._.config.paths.mocha = 'thirdParty/mocha';
-    requirejs.s.contexts._.config.paths.sinon = 'thirdParty/sinon';
+    requirejs.s.contexts._.config.paths.chai = 'thirdParty/test/chai';
+    requirejs.s.contexts._.config.paths.mocha = 'thirdParty/test/mocha';
+    requirejs.s.contexts._.config.paths.sinon = 'thirdParty/test/sinon';
 
     requirejs.s.contexts._.config.shim.mocha = {
         exports: 'window.mocha'
