42798c245489ec4e952eea7c848280c1f159f76b
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2015-08-03 12:22:48 -0700

    Add a disconnect method to assist with testing.

--- src/js/contentScript/youTubePlayer/model/port.js
+++ src/js/contentScript/youTubePlayer/model/port.js
@@ -12,6 +12,7 @@
       this._onPortMessage = this._onPortMessage.bind(this);
     },
 
+    // Utilize Chrome's API to establish a connection from the YouTube page back to Streamus.
     connect: function() {
       if (!this.get('isConnected')) {
         this._port = chrome.runtime.connect({
@@ -23,6 +24,17 @@
       }
     },
 
+    // Break an existing connection by disconnecting the port.
+    disconnect: function() {
+      if (this.get('isConnected')) {
+        this._port.onMessage.removeListener(this._onPortMessage);
+        this._port.disconnect();
+        this._port = null;
+        this.set('isConnected', false);
+      }
+    },
+
+    // Send a message through the provided Chrome port back to the Streamus extension.
     postMessage: function(message) {
       if (this.get('isConnected')) {
         this._port.postMessage(message);
