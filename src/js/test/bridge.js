3e896cb716b8cb0a3f3fa29d022e35f42d975c0c
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2015-09-05 22:20:53 -0700

    Remove mocha css; loaded via jspm

--- src/js/test/bridge.js
+++ src/js/test/bridge.js
@@ -12,10 +12,9 @@
 (function() {
   // Only run when executing under phantom.js browser.
   if (!window.PHANTOMJS) {
-    console.log('returning');
     return;
   }
-  console.log('go!');
+
   // Send messages to the parent phantom.js process via alert! Good times!!
   function sendMessage() {
     var args = [].slice.call(arguments);
